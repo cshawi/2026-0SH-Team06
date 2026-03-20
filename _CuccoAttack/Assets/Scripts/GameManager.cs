55419e3e2d19669ed79dab4689c304f12b6b94a5
Sacha Lamarche <sachalamarche@yahoo.com>
2026-03-20 11:27:30 -0400

    Commencé structure GameManager et ajouté .gitignore pour builds futurs

--- _CuccoAttack/Assets/Scripts/GameManager.cs
+++ _CuccoAttack/Assets/Scripts/GameManager.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance {get; private set;}
 
+    //public int world {get; private set;}
+    public int stage {get; private set;}
+    public int lives {get; private set;}
     private void Awake()
     {
         if (Instance != null)
@@ -13,5 +17,39 @@ public class GameManager : MonoBehaviour
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        Instance = null;
+    }
+
+    private void Start()
+    {
+        NewGame();
+    }
+    private void NewGame()
+    {
+        lives = 3;
+        LoadLevel();
+    }
+
+    private void LoadLevel(int stage)
+    {
+        //this.world = world;
+        this.stage = stage;
+
+        SceneManager.LoadScene("stage");
+    }
+
+    public void NextLevel()
+    {
+        LoadLevel(stage + 1);
+    }
+
+    private void GameOver()
+    {
+        SceneManager.LoadScene("GameOver");
     }
 }
